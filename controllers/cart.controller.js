06b840853935f591d9e0721fa6702bc9c389fc9a
Noureddine Laassari <nlaassari@gmail.com>
2023-10-26 04:27:42 +0100

    Fix cart error handling

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -8,25 +8,37 @@ export const addToCart = async (req, res) => {
   const productId = req.body.productId;
   const CartInstance = new Cart(req.session);
 
-  const cart = await CartInstance.addToCart(productId);
-
-  res.send(cart);
+  try {
+    const cart = await CartInstance.addToCart(productId);
+    res.send(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 };
 
 export const updateCart = (req, res) => {
   const { cartItemId, quantity } = req.body;
   const CartInstance = new Cart(req.session);
 
-  const cart = CartInstance.updateCart(cartItemId, quantity);
-
-  res.send(cart);
+  try {
+    const cart = CartInstance.updateCart(cartItemId, quantity);
+    res.send(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 };
 
 export const removeFromCart = (req, res) => {
   const { cartItemId } = req.params;
   const CartInstance = new Cart(req.session);
 
-  const cart = CartInstance.removeFromCart(cartItemId);
-
-  res.send(cart);
+  try {
+    const cart = CartInstance.removeFromCart(cartItemId);
+    res.send(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 };
